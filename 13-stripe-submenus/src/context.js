11d98e6f30ce489edc96b6c3cb3cef1d0c713ef6
Heby T Paul <hebytpaul1111@gmail.com>
2023-04-21 19:41:42 +0530

    Stripe-submenus Complete

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,2 +1,47 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({})
+  const [page, setPage] = useState({page:'',links: [] })
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        location,
+        page,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
